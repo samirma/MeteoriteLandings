c4583866cb9193b6b5f755fa89c651183864b467
Samir Antonio <samir.ma@gmail.com>
2016-12-18 09:53:58 -0200

    feat: Models became Parcelable
    Added Parcelable feature on Geolocation and Meteorite models

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/model/Geolocation.java
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/model/Geolocation.java
@@ -1,7 +1,9 @@
 package com.antonio.samir.meteoritelandingsspots.model;
 
-public class Geolocation
-{
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Geolocation implements Parcelable {
     private String type;
 
     private String[] coordinates;
@@ -27,4 +29,34 @@ public class Geolocation
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.type);
+        dest.writeStringArray(this.coordinates);
+    }
+
+    public Geolocation() {
+    }
+
+    protected Geolocation(Parcel in) {
+        this.type = in.readString();
+        this.coordinates = in.createStringArray();
+    }
+
+    public static final Parcelable.Creator<Geolocation> CREATOR = new Parcelable.Creator<Geolocation>() {
+        @Override
+        public Geolocation createFromParcel(Parcel source) {
+            return new Geolocation(source);
+        }
+
+        @Override
+        public Geolocation[] newArray(int size) {
+            return new Geolocation[size];
+        }
+    };
 }
