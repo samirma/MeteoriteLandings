cd13e5bce0c8cbb518bdc82ea018c113b04ead23
Samir Antonio <samir.ma@gmail.com>
2017-05-30 23:15:05 -0300

    feat: landscape animation

---
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/ui/animator/AnimateParam.java
@@ -0,0 +1,14 @@
+package com.antonio.samir.meteoritelandingsspots.ui.animator;
+
+import android.content.Context;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.Interpolator;
+
+public class AnimateParam {
+    //public Photo photo;
+    public int width;
+    public Context context;
+    public Interpolator interpolator;
+    public ViewPropertyAnimator endAnimate;
+    public ViewPropertyAnimator startAnimate;
+}
