3ae6255363af248591dc5a3d8c78acf8571d8aa7
samir.antonio <samir.antonio@farfetch.com>
2017-07-10 10:57:30 +0100

    migration added

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/service/repository/MeteoriteRepository.java
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/service/repository/MeteoriteRepository.java
@@ -1,17 +1,29 @@
 package com.antonio.samir.meteoritelandingsspots.service.repository;
 
-import android.arch.persistence.room.Room;
-import android.content.Context;
-
 import com.antonio.samir.meteoritelandingsspots.service.repository.database.AppDatabase;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.migration.Migration;
+import android.content.Context;
 
 public class MeteoriteRepository {
 
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE meteorites "
+                    + " ADD COLUMN address VARCHAR");
+
+            database.execSQL("UPDATE meteorites SET address = (SELECT address FROM address WHERE address._id = _id)");
+
+        }
+    };
     private final AppDatabase appDatabase;
 
     public MeteoriteRepository(Context context) {
         appDatabase = Room.databaseBuilder(context,
-                AppDatabase.class, "meteorites").build();
+                AppDatabase.class, "meteorites").addMigrations(MIGRATION_1_2).build();
 
     }
 
