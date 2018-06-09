629621bf4432b2660accf5f17e103a204498be5b
Samir Antonio <samir.ma@gmail.com>
2018-06-09 14:30:53 +0100

    chore: convert MeteoriteSelector to Kotlin

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/ui/recyclerView/selector/MeteoriteSelectorLandscape.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/ui/recyclerView/selector/MeteoriteSelectorLandscape.kt
@@ -3,7 +3,7 @@ package com.antonio.samir.meteoritelandingsspots.ui.recyclerView.selector
 
 class MeteoriteSelectorLandscape(private val view: MeteoriteSelectorView) : MeteoriteSelector {
 
-    override fun selectItemId(meteorite: String) {
+    override fun selectItemId(meteorite: String?) {
         view.selectLandscape(meteorite)
     }
 
