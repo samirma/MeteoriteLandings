6d5fd44ed86e95937eea7001126ce90517199603
Samir Antonio <samir.ma@gmail.com>
2019-06-19 19:20:55 +0100

    feat: add coroutines to MeteoriteDetailViewModel

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/detail/viewmodel/MeteoriteDetailViewModel.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/detail/viewmodel/MeteoriteDetailViewModel.kt
@@ -3,19 +3,24 @@ package com.antonio.samir.meteoritelandingsspots.features.detail.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
-import com.antonio.samir.meteoritelandingsspots.model.Meteorite
-import com.antonio.samir.meteoritelandingsspots.service.repository.MeteoriteRepositoryInterface
+import androidx.lifecycle.viewModelScope
+import com.antonio.samir.meteoritelandingsspots.service.business.MeteoriteServiceInterface
+import com.antonio.samir.meteoritelandingsspots.service.business.model.Meteorite
+import kotlinx.coroutines.launch
+
 
 class MeteoriteDetailViewModel(
-        private val meteoriteRepository: MeteoriteRepositoryInterface
+        private val meteoriteService: MeteoriteServiceInterface
 ) : ViewModel() {
 
     private val meteorite: MediatorLiveData<Meteorite> = MediatorLiveData()
 
     fun loadMeteoriteById(meteoriteId: String) {
-        meteoriteRepository.getMeteoriteById(meteoriteId)?.let {
-            meteorite.addSource(it) { value ->
-                meteorite.setValue(value)
+        viewModelScope.launch {
+            meteoriteService.getMeteoriteById(meteoriteId)?.let {
+                meteorite.addSource(it) { value ->
+                    meteorite.setValue(value)
+                }
             }
         }
     }
