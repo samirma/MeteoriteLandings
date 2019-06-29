96c40b765139db13886ef04cf775d832970ca646
Samir Antonio <samir.ma@gmail.com>
2019-06-29 13:06:37 +0100

    feat: only update the view with a new data

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/service/business/model/Meteorite.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/service/business/model/Meteorite.kt
@@ -58,4 +58,36 @@ class Meteorite constructor() : Parcelable {
             return yearParsed
         }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Meteorite
+
+        if (id != other.id) return false
+        if (mass != other.mass) return false
+        if (nametype != other.nametype) return false
+        if (recclass != other.recclass) return false
+        if (name != other.name) return false
+        if (fall != other.fall) return false
+        if (year != other.year) return false
+        if (reclong != other.reclong) return false
+        if (reclat != other.reclat) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + (mass?.hashCode() ?: 0)
+        result = 31 * result + (nametype?.hashCode() ?: 0)
+        result = 31 * result + (recclass?.hashCode() ?: 0)
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (fall?.hashCode() ?: 0)
+        result = 31 * result + (year?.hashCode() ?: 0)
+        result = 31 * result + (reclong?.hashCode() ?: 0)
+        result = 31 * result + (reclat?.hashCode() ?: 0)
+        return result
+    }
+
 }
