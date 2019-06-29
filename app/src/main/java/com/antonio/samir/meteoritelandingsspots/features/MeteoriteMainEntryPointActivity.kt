4b6193153d2c3b058578e9215ea809eb44689ec8
Samir Antonio <samir.ma@gmail.com>
2019-06-29 18:11:36 +0100

    feat: moving away from a fat activity

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/MeteoriteMainEntryPointActivity.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/MeteoriteMainEntryPointActivity.kt
@@ -1,45 +1,14 @@
 package com.antonio.samir.meteoritelandingsspots.features
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.antonio.samir.meteoritelandingsspots.R
-import com.antonio.samir.meteoritelandingsspots.features.detail.ui.MeteoriteDetailFragment
-import kotlinx.android.synthetic.main.activity_meteorite_list.*
 
 class MeteoriteMainEntryPointActivity : AppCompatActivity() {
-
-    private var selectedMeteorite: String? = null
-
-    private var meteoriteDetailFragment: MeteoriteDetailFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meteorite_list)
-    }
-
-    fun selectMeteoriteLandscape(meteoriteId: String) {
-
-        if (meteoriteDetailFragment == null) {
-
-            fragment?.visibility = View.VISIBLE
-
-            var fragmentTransaction = supportFragmentManager.beginTransaction()
-
-            fragmentTransaction = fragmentTransaction.setCustomAnimations(
-                    R.anim.fragment_slide_left_enter,
-                    R.anim.fragment_slide_left_exit)
-
-            meteoriteDetailFragment = MeteoriteDetailFragment.newInstance(meteoriteId)
-            fragmentTransaction.replace(R.id.fragment, meteoriteDetailFragment!!)
-            fragmentTransaction.commit()
-
-        } else {
-            meteoriteDetailFragment?.setCurrentMeteorite(meteoriteId)
-        }
-
-        selectedMeteorite = meteoriteId
-
     }
 
 }
