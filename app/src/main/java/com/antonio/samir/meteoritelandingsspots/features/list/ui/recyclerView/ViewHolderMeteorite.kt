5e0eed5e6a36f9d75be7ea74012ca693690e68c5
Samir Antonio <samir.ma@gmail.com>
2019-12-18 22:45:45 +0000

    feat: move code from MeteoriteAdapter to ViewHolderMeteorite

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/ui/recyclerView/ViewHolderMeteorite.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/ui/recyclerView/ViewHolderMeteorite.kt
@@ -1,22 +1,30 @@
 package com.antonio.samir.meteoritelandingsspots.features.list.ui.recyclerView
 
+import android.location.Location
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.antonio.samir.meteoritelandingsspots.R
+import com.antonio.samir.meteoritelandingsspots.features.getDistanceFrom
+import com.antonio.samir.meteoritelandingsspots.features.yearString
 import com.antonio.samir.meteoritelandingsspots.service.business.model.Meteorite
+import org.apache.commons.lang3.StringUtils
+import java.util.*
 
 
 class ViewHolderMeteorite(view: View) : RecyclerView.ViewHolder(view) {
 
-    var name: TextView
+    val context = view.context
 
-    var location: TextView
+    var name: TextView = view.findViewById(R.id.title)
 
-    var mCardview: CardView
+    var addressTV: TextView = view.findViewById(R.id.location)
+
+    var cardView: CardView = view.findViewById(R.id.cardview)
 
     var addressObserver: Observer<Meteorite>? = null
 
@@ -24,16 +32,73 @@ class ViewHolderMeteorite(view: View) : RecyclerView.ViewHolder(view) {
 
     var meteorite: Meteorite? = null
 
-    init {
+    fun onBind(meteorite: Meteorite, selectedMeteorite: Meteorite?, location: Location?) {
 
-        name = view.findViewById(R.id.title)
-
-        location = view.findViewById(R.id.location)
-
-        mCardview = view.findViewById(R.id.cardview)
-
+        if (this.meteorite != meteorite) {
+            this.meteorite = meteorite
+            populateViewHolder(meteorite, location, selectedMeteorite)
+        } else {
+            val newAddress = this.meteorite?.address
+            if (StringUtils.isNotEmpty(newAddress)) {
+                showAddress(newAddress!!, meteorite, location)
+                this.addressTV.startAnimation(AnimationUtils.loadAnimation(context, R.anim.view_show))
+                liveMet?.removeObserver(addressObserver!!)
+            }
+        }
 
     }
 
+    private fun populateViewHolder(meteorite: Meteorite, location: Location?, selectedMeteorite: Meteorite?) {
+        val meteoriteName = meteorite.name
 
+        val year = meteorite.yearString
+
+        name.text = context.getString(R.string.title, meteoriteName, year)
+
+        name.contentDescription = meteoriteName
+
+        setLocationText(meteorite, location)
+
+        var color = R.color.unselected_item_color
+        var title_color = R.color.title_color
+        var elevation = R.dimen.unselected_item_elevation
+
+        if (Objects.equals(meteorite, selectedMeteorite)) {
+            color = R.color.selected_item_color
+            title_color = R.color.selected_title_color
+            elevation = R.dimen.selected_item_elevation
+        }
+
+        cardView.setCardBackgroundColor(context.resources.getColor(color))
+        cardView.cardElevation = context.resources.getDimensionPixelSize(elevation).toFloat()
+        name.setTextColor(context.resources.getColor(title_color))
+    }
+
+
+    private fun setLocationText(meteorite: Meteorite, location: Location?) {
+        val address = meteorite.address
+
+        //Always remove the previous observer
+        if (liveMet != null && addressObserver != null) {
+            liveMet?.removeObserver(addressObserver!!)
+            liveMet = null
+            addressObserver = null
+        }
+
+        if (StringUtils.isNotEmpty(address)) {
+            showAddress(address, meteorite, location)
+        } else {
+            this.addressTV.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun showAddress(address: String?, meteorite: Meteorite, location: Location?) {
+        val finalAddress = address + if (location != null) {
+            " - ${meteorite.getDistanceFrom(location.latitude, location.longitude)}"
+        } else {
+            ""
+        }
+        addressTV.text = finalAddress
+        addressTV.visibility = View.VISIBLE
+    }
 }
