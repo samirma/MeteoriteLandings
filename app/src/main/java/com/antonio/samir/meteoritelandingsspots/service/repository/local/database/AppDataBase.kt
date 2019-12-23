384e16117b8acfdc057ed970801ac52446fb99a9
Samir Moreira Antonio <samir.ma@gmail.com>
2019-12-23 11:49:55 +0000

    fix database crash

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/service/repository/local/database/AppDataBase.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/service/repository/local/database/AppDataBase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.antonio.samir.meteoritelandingsspots.service.business.model.Meteorite
 
-@Database(entities = arrayOf(Meteorite::class), version = 2)
+@Database(entities = arrayOf(Meteorite::class), version = 3)
 abstract class AppDataBase : RoomDatabase() {
 
     abstract fun meteoriteDao(): MeteoriteDao
