817f389e3437db80ac7aa84b2f6495eda3079d43
Samir Antonio <samir.ma@gmail.com>
2020-04-18 23:20:01 +0100

    feat: update GPSTrackerInterface to flow

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/util/GPSTrackerInterface.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/util/GPSTrackerInterface.kt
@@ -2,14 +2,13 @@ package com.antonio.samir.meteoritelandingsspots.util
 
 import android.location.Location
 import android.location.LocationListener
-import com.antonio.samir.meteoritelandingsspots.data.Result
 import kotlinx.coroutines.flow.Flow
 
 interface GPSTrackerInterface : LocationListener {
 
     val isLocationAuthorized: Boolean
 
-    val location: Flow<Result<Location>>
+    val location: Flow<Location>
 
     suspend fun startLocationService()
 
