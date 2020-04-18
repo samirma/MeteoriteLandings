76e57a279802f9f814846c3fe1ce039dacb647e7
Samir Antonio <samir.ma@gmail.com>
2020-04-18 21:53:19 +0100

    chore: move class

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/repository/MeteoriteServerException.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/repository/MeteoriteServerException.kt
@@ -1,4 +1,4 @@
-package com.antonio.samir.meteoritelandingsspots.service.business
+package com.antonio.samir.meteoritelandingsspots.data.repository
 
 /**
  * Server Exception created in order to prevent the ui layer be aware the exception of service layer
