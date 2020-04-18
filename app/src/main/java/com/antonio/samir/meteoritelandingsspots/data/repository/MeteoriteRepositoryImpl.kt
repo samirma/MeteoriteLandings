dd4a8a432e6f1628347c3a3f6f3095c12649de21
Samir Antonio <samir.ma@gmail.com>
2020-04-18 23:53:46 +0100

    feat: update AddressService to work with flow

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/repository/MeteoriteRepositoryImpl.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/repository/MeteoriteRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.antonio.samir.meteoritelandingsspots.data.repository
 
 import androidx.paging.DataSource
 import com.antonio.samir.meteoritelandingsspots.data.Result
-import com.antonio.samir.meteoritelandingsspots.data.local.MeteoriteRepositoryInterface
+import com.antonio.samir.meteoritelandingsspots.data.local.MeteoriteLocalRepository
 import com.antonio.samir.meteoritelandingsspots.data.repository.model.Meteorite
 import com.antonio.samir.meteoritelandingsspots.util.DefaultDispatcherProvider
 import com.antonio.samir.meteoritelandingsspots.util.DispatcherProvider
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 
 
 class MeteoriteRepositoryImpl(
-        private val meteoriteRepository: MeteoriteRepositoryInterface,
+        private val meteoriteLocalRepository: MeteoriteLocalRepository,
         private val dispatchers: DispatcherProvider = DefaultDispatcherProvider()
 ) : MeteoriteRepository {
 
@@ -19,16 +19,15 @@ class MeteoriteRepositoryImpl(
     }
 
     override fun getMeteoriteById(id: String): Flow<Result<Meteorite>> {
+        return meteoriteLocalRepository.getMeteoriteById(id)
+    }
+
+    override suspend fun updateAddress(meteorite: Meteorite) {
         TODO("Not yet implemented")
     }
 
-    override suspend fun requestAddressUpdate(meteorite: Meteorite) {
+    override suspend fun updateAddress(list: List<Meteorite>) {
         TODO("Not yet implemented")
     }
-
-    override fun requestAddressUpdate(list: List<Meteorite>) {
-        TODO("Not yet implemented")
-    }
-
 
 }
