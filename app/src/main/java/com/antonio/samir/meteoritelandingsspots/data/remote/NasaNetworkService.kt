fc6276d71f037896c24be471a26cc9ff9094d190
Samir Antonio <samir.ma@gmail.com>
2020-04-20 21:58:17 +0100

    feat: remove flow from NasaNetworkService

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/remote/NasaNetworkService.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/remote/NasaNetworkService.kt
@@ -1,21 +1,11 @@
 package com.antonio.samir.meteoritelandingsspots.data.remote
 
-import com.antonio.samir.meteoritelandingsspots.data.Result
-import com.antonio.samir.meteoritelandingsspots.data.repository.MeteoriteServerException
 import com.antonio.samir.meteoritelandingsspots.data.repository.model.Meteorite
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import java.io.IOException
 
 class NasaNetworkService(val service: NasaServerEndPoint) : NetworkService {
 
-    override suspend fun getMeteorites(limit: Int, offset: Int): Flow<Result<List<Meteorite>>> = flow {
-        emit(Result.InProgress<List<Meteorite>>())
-        try {
-            emit(Result.Success(service.publicMeteorites(limit, offset)))
-        } catch (e: IOException) {
-            emit(Result.Error(MeteoriteServerException(e)))
-        }
+    override suspend fun getMeteorites(limit: Int, offset: Int): List<Meteorite> {
+        return service.publicMeteorites(limit, offset)
     }
 
 }
