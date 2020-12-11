f51e5ba18388adde73573022bf657021adef19a9
Samir <samir.ma@gmail.com>
2020-12-11 21:19:41 +0000

    add actionbar

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/MeteoriteMainEntryPointActivity.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/MeteoriteMainEntryPointActivity.kt
@@ -1,7 +1,43 @@
 package com.antonio.samir.meteoritelandingsspots.features
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.antonio.samir.meteoritelandingsspots.R
 
+class MeteoriteMainEntryPointActivity : AppCompatActivity(R.layout.activity_meteorite_list) {
 
-class MeteoriteMainEntryPointActivity : AppCompatActivity(R.layout.activity_meteorite_list)
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meteorite_list)
+        configureActionBar()
+    }
+
+    private fun configureActionBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.meteoriteListFragment)
+        )
+
+        val navController = getNavController()
+
+        navController.setGraph(R.navigation.nav_main, intent.extras)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    private fun getNavController() =
+            (supportFragmentManager.findFragmentById(R.id.main_fragment_container_view)
+                    as NavHostFragment).navController
+
+}
---
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/ui/extension/FragmentExtension.kt
@@ -0,0 +1,25 @@
+package com.antonio.samir.meteoritelandingsspots.ui.extension
+
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.antonio.samir.meteoritelandingsspots.R
+import com.google.android.material.appbar.AppBarLayout
+
+fun Fragment.hideActionBar() {
+    with(requireActivity()) {
+        val appBarLayoutFlowMain = findViewById<AppBarLayout>(R.id.app_bar_flow)
+        appBarLayoutFlowMain?.visibility = View.GONE
+    }
+}
+
+fun Fragment.showActionBar(title: CharSequence? = null) {
+    with(requireActivity()) {
+        val textActionBarTitle = findViewById<TextView>(R.id.toolbar_title)
+        val appBarLayoutFlowMain = findViewById<AppBarLayout>(R.id.app_bar_flow)
+        title?.let {
+            textActionBarTitle?.text = it
+        }
+        appBarLayoutFlowMain?.visibility = View.VISIBLE
+    }
+}
