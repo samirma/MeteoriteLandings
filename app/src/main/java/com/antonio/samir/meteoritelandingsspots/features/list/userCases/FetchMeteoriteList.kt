7a4cac9f7a26f8df98dab1851d27ab46cb19c0fe
Samir Antonio <samir.ma@gmail.com>
2022-01-22 19:02:20 +0000

    code style

---
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/FetchMeteoriteList.kt
@@ -0,0 +1,13 @@
+package com.antonio.samir.meteoritelandingsspots.features.list.userCases
+
+import com.antonio.samir.meteoritelandingsspots.common.ResultOf
+import com.antonio.samir.meteoritelandingsspots.common.userCase.UserCaseBase
+import com.antonio.samir.meteoritelandingsspots.data.repository.MeteoriteRepository
+
+class FetchMeteoriteList(
+    private val meteoriteRepository: MeteoriteRepository,
+) : UserCaseBase<Unit, ResultOf<Unit>>() {
+
+    override fun action(input: Unit) = meteoriteRepository.loadDatabase()
+
+}
