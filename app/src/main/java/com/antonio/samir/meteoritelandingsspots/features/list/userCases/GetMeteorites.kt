5ac25a26b3b1500c18db1b2f2ecc722e2e4ba551
Samir Antonio <samir.ma@gmail.com>
2022-01-22 19:56:49 +0000

    code style

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/GetMeteorites.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/GetMeteorites.kt
@@ -1,6 +1,7 @@
 package com.antonio.samir.meteoritelandingsspots.features.list.userCases
 
 import android.location.Location
+import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -8,8 +9,10 @@ import androidx.paging.map
 import com.antonio.samir.meteoritelandingsspots.common.userCase.UserCaseBase
 import com.antonio.samir.meteoritelandingsspots.data.local.MeteoriteLocalRepository
 import com.antonio.samir.meteoritelandingsspots.features.list.MeteoriteItemView
+import com.antonio.samir.meteoritelandingsspots.features.list.MeteoriteListFragment
+import com.antonio.samir.meteoritelandingsspots.features.list.MeteoriteListViewModel
 import com.antonio.samir.meteoritelandingsspots.features.list.mapper.MeteoriteViewMapper
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 
 class GetMeteorites(
     private val meteoriteLocalRepository: MeteoriteLocalRepository,
@@ -39,6 +42,7 @@ class GetMeteorites(
         }
 
     companion object {
+        private val TAG = GetMeteorites::class.java.simpleName
         const val PAGE_SIZE = 30
         const val LIMIT = 1000L
     }
