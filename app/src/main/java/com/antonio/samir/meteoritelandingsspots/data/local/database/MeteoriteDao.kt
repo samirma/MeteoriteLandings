80cc42251fe2efd7587b2799ba8201585a1c4d38
Samir Antonio <samir.ma@gmail.com>
2022-03-01 19:12:21 +0000

    add distance

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/local/database/MeteoriteDao.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/data/local/database/MeteoriteDao.kt
@@ -10,25 +10,25 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface MeteoriteDao {
 
-    @Query("SELECT * from meteorites ORDER BY name LIMIT :limit")
+    @Query("SELECT * from meteorites WHERE recLong IS NOT NULL ORDER BY name LIMIT :limit")
     fun meteoriteOrdered(limit: Long): PagingSource<Int, Meteorite>
 
-    @Query("SELECT * from meteorites ORDER BY ((reclat-:lat)*(reclat-:lat)) + ((reclong - :lng)*(reclong - :lng)) ASC")
+    @Query("SELECT * from meteorites WHERE recLong IS NOT NULL ORDER BY ((reclat-:lat)*(reclat-:lat)) + ((reclong - :lng)*(reclong - :lng)) ASC")
     fun meteoriteOrderedByLocation(lat: Double, lng: Double): PagingSource<Int, Meteorite>
 
-    @Query("SELECT * from meteorites WHERE (LOWER(address) GLOB '*' || :filter|| '*') or (LOWER(name) GLOB '*' || :filter|| '*') ORDER BY ((reclat-:lat)*(reclat-:lat)) + ((reclong - :lng)*(reclong - :lng)) ASC")
+    @Query("SELECT * from meteorites WHERE recLong IS NOT NULL AND ((LOWER(address) GLOB '*' || :filter|| '*') or (LOWER(name) GLOB '*' || :filter|| '*')) ORDER BY ((reclat-:lat)*(reclat-:lat)) + ((reclong - :lng)*(reclong - :lng)) ASC")
     fun meteoriteOrderedByLocationFiltered(lat: Double, lng: Double, filter: String): PagingSource<Int, Meteorite>
 
-    @Query("SELECT * from meteorites WHERE (LOWER(address) GLOB '*' || :filter|| '*') or (LOWER(name) GLOB '*' || :filter|| '*') ORDER BY name ASC LIMIT 5000")
+    @Query("SELECT * from meteorites WHERE recLong IS NOT NULL AND ((LOWER(address) GLOB '*' || :filter|| '*') or (LOWER(name) GLOB '*' || :filter|| '*')) ORDER BY name ASC LIMIT 5000")
     fun meteoriteFiltered(filter: String): PagingSource<Int, Meteorite>
 
-    @Query("SELECT * from meteorites WHERE address IS NULL OR LENGTH(address) = 0 ORDER BY id LIMIT 30")
+    @Query("SELECT * from meteorites WHERE recLong IS NOT NULL AND (address IS NULL OR LENGTH(address) = 0) ORDER BY id LIMIT 30")
     fun meteoritesWithOutAddress(): Flow<List<Meteorite>>
 
-    @Query("SELECT * from meteorites where id = :meteoriteId LIMIT 1")
+    @Query("SELECT * from meteorites WHERE id = :meteoriteId LIMIT 1")
     fun getMeteoriteById(meteoriteId: String): Flow<Meteorite>
 
-    @Query("SELECT count(id) from meteorites")
+    @Query("SELECT count(id) from meteorites WHERE recLong IS NOT NULL")
     suspend fun getMeteoritesCount(): Int
 
     @Query("SELECT count(id) from meteorites WHERE address IS NULL OR LENGTH(address) = 0")
