f09285ebf1b5b4fa08fb0f3b98e3918850e65f1e
Samir Antonio <samir.ma@gmail.com>
2022-03-23 20:01:13 +0000

    start address recover after recover all meteorites

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/FetchMeteoriteList.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/FetchMeteoriteList.kt
@@ -3,11 +3,17 @@ package com.antonio.samir.meteoritelandingsspots.features.list.userCases
 import com.antonio.samir.meteoritelandingsspots.common.ResultOf
 import com.antonio.samir.meteoritelandingsspots.common.userCase.UserCaseBase
 import com.antonio.samir.meteoritelandingsspots.data.repository.MeteoriteRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.onEach
 
+@FlowPreview
 class FetchMeteoriteList(
     private val meteoriteRepository: MeteoriteRepository,
+    private val startAddressRecover: StartAddressRecover,
 ) : UserCaseBase<Unit, ResultOf<Unit>>() {
 
-    override fun action(input: Unit) = meteoriteRepository.loadDatabase()
+    override fun action(input: Unit) = meteoriteRepository.loadDatabase().onEach {
+        if (it is ResultOf.Success) startAddressRecover.execute(Unit)
+    }
 
 }
