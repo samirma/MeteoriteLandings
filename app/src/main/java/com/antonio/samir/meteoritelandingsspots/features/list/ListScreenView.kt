619dc35113d833c0c2d228019a7d709a4f1f4364
Samir Antonio <samir.ma@gmail.com>
2023-08-20 17:50:12 +0100

    update isDark

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/ListScreenView.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/ListScreenView.kt
@@ -3,17 +3,13 @@ package com.antonio.samir.meteoritelandingsspots.features.list
 import androidx.annotation.StringRes
 import androidx.paging.PagingData
 import com.antonio.samir.meteoritelandingsspots.common.ResultOf
-import com.antonio.samir.meteoritelandingsspots.designsystem.ui.components.HeaderState
 import com.antonio.samir.meteoritelandingsspots.designsystem.ui.components.MeteoriteItemView
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 data class ListScreenView(
-    val isDark: StateFlow<Boolean> = MutableStateFlow(true),
+    val isDark: Boolean,
     val addressStatus: ResultOf<Float>,
     val onDarkModeToggleClick: () -> Unit,
-    val headerState: HeaderState = HeaderState.Expanded,
     val listState: ListState
 )
 
