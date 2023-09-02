a0794dd06641ab61baf57ccaa89bee9d577964c1
Samir Antonio <samir.ma@gmail.com>
2023-09-02 13:00:59 +0100

    DebugScreen draft

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/FetchMeteoriteList.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/userCases/FetchMeteoriteList.kt
@@ -3,9 +3,7 @@ package com.antonio.samir.meteoritelandingsspots.features.list.userCases
 import com.antonio.samir.meteoritelandingsspots.common.ResultOf
 import com.antonio.samir.meteoritelandingsspots.common.userCase.UserCaseBase
 import com.antonio.samir.meteoritelandingsspots.data.repository.MeteoriteRepository
-import kotlinx.coroutines.FlowPreview
 
-@FlowPreview
 class FetchMeteoriteList(
     private val meteoriteRepository: MeteoriteRepository,
 ) : UserCaseBase<Unit, ResultOf<Unit>>() {
