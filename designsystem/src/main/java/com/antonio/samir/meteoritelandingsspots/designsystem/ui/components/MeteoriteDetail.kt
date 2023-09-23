269be0f2a2c90602d263568583f50d7ab7587bb4
Samir Antonio <samir.ma@gmail.com>
2023-09-23 21:56:12 +0100

    update libs

--- designsystem/src/main/java/com/antonio/samir/meteoritelandingsspots/designsystem/ui/components/MeteoriteDetail.kt
+++ designsystem/src/main/java/com/antonio/samir/meteoritelandingsspots/designsystem/ui/components/MeteoriteDetail.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,6 +23,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antonio.samir.meteoritelandingsspots.designsystem.R
 import com.antonio.samir.meteoritelandingsspots.designsystem.ui.theme.ExtendedTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 
 data class MeteoriteView(
@@ -40,6 +47,9 @@ data class MeteoriteView(
 fun MeteoriteDetail(
     meteoriteView: MeteoriteView
 ) {
+    val latitude = meteoriteView.reclat.toDouble()
+    val longitude = meteoriteView.reclong.toDouble()
+
     Surface(
         modifier = Modifier.background(MaterialTheme.colorScheme.background)
     ) {
@@ -49,6 +59,21 @@ fun MeteoriteDetail(
                 .padding(vertical = 16.dp),
             verticalArrangement = Arrangement.Center,
         ) {
+
+            // Google Map view with a marker at the given location
+            val target = LatLng(latitude, longitude)
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(target, 10f)
+            }
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState
+            ) {
+                Marker(
+                    state = MarkerState(position = target)
+                )
+            }
+
             LineDetail(
                 icon = R.drawable.ic_globe,
                 label = meteoriteView.address,
