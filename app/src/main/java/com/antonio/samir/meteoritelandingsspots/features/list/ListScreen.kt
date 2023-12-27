c020cbbafbd846d63b487c8e5aafa2c93cd68d22
Samir Antonio <samir.ma@gmail.com>
2023-12-27 19:26:43 +0000

    remove Shimmer and improved MeteoriteItem

--- app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/ListScreen.kt
+++ app/src/main/java/com/antonio/samir/meteoritelandingsspots/features/list/ListScreen.kt
@@ -2,10 +2,13 @@ package com.antonio.samir.meteoritelandingsspots.features.list
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
@@ -14,8 +17,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -172,6 +177,16 @@ fun TextLazyColumn() {
         items.forEach {
             item {
                 MeteoriteItem(it, null)
+                Image(
+                    painter = painterResource(id = R.drawable.divider),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = 16.dp,
+                            bottom = 1.dp
+                        )
+                )
             }
         }
 
