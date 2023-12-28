e07a9bff2a26647a55cb6e84900724096dace497
Samir Antonio <samir.ma@gmail.com>
2023-12-28 17:54:10 +0000

    fix GoogleMap size in the MeteoriteDetail

--- designsystem/src/main/java/com/antonio/samir/meteoritelandingsspots/designsystem/ui/components/MeteoriteDetail.kt
+++ designsystem/src/main/java/com/antonio/samir/meteoritelandingsspots/designsystem/ui/components/MeteoriteDetail.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antonio.samir.meteoritelandingsspots.designsystem.R
 import com.antonio.samir.meteoritelandingsspots.designsystem.ui.theme.ExtendedTheme
+import com.antonio.samir.meteoritelandingsspots.designsystem.ui.theme.MeteoriteLandingsTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -49,35 +50,32 @@ fun MeteoriteDetail(
     val latitude = meteoriteView.reclat
     val longitude = meteoriteView.reclong
 
-    Surface(
-        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .padding(vertical = 16.dp),
+        verticalArrangement = Arrangement.Center,
     ) {
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.background)
-                .padding(vertical = 16.dp),
-            verticalArrangement = Arrangement.Center,
-        ) {
 
-            // Google Map view with a marker at the given location
-            val target = LatLng(latitude, longitude)
-            val cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(target, 10f)
-            }
-            GoogleMap(
-                modifier = Modifier.fillMaxSize(),
-                cameraPositionState = cameraPositionState
-            ) {
-                Marker(
-                    state = MarkerState(position = target)
-                )
-            }
-
-            LineDetail(icon = R.drawable.ic_globe, label = meteoriteView.address)
-            LineDetail(icon = R.drawable.ic_weight, label = meteoriteView.mass)
-            LineDetail(icon = R.drawable.ic_type, label = meteoriteView.type)
-            LineDetail(icon = R.drawable.ic_crash, label = meteoriteView.yearString)
+        // Google Map view with a marker at the given location
+        val target = LatLng(latitude, longitude)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(target, 10f)
         }
+        GoogleMap(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),  // This makes the GoogleMap fill all available space
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = target)
+            )
+        }
+        LineDetail(icon = R.drawable.ic_globe, label = meteoriteView.address)
+        LineDetail(icon = R.drawable.ic_weight, label = meteoriteView.mass)
+        LineDetail(icon = R.drawable.ic_type, label = meteoriteView.type)
+        LineDetail(icon = R.drawable.ic_crash, label = meteoriteView.yearString)
     }
 }
 
@@ -115,18 +113,22 @@ private fun LineDetail(
 @Composable
 fun MeteoriteDetailDark() {
     val sample = "test"
-    MeteoriteDetail(
-        meteoriteView = MeteoriteView(
-            id = sample,
-            name = "name $sample",
-            yearString = "yearString $sample",
-            address = "address $sample",
-            type = "distance $sample",
-            mass = "mass",
-            reclat = 0.0,
-            reclong = 0.0
+    MeteoriteLandingsTheme(
+        darkTheme = true
+    ) {
+        MeteoriteDetail(
+            meteoriteView = MeteoriteView(
+                id = sample,
+                name = "name $sample",
+                yearString = "yearString $sample",
+                address = "address $sample",
+                type = "distance $sample",
+                mass = "mass",
+                reclat = 0.0,
+                reclong = 0.0
+            )
         )
-    )
+    }
 }
 
 
@@ -134,36 +136,20 @@ fun MeteoriteDetailDark() {
 @Composable
 fun MeteoriteDetailLight() {
     val sample = "test"
-    MeteoriteDetail(
-        meteoriteView = MeteoriteView(
-            id = sample,
-            name = "name $sample",
-            yearString = "yearString $sample",
-            address = "address $sample",
-            type = "distance $sample",
-            mass = "mass",
-            reclat = 0.0,
-            reclong = 0.0
+    MeteoriteLandingsTheme(
+        darkTheme = false
+    ) {
+        MeteoriteDetail(
+            meteoriteView = MeteoriteView(
+                id = sample,
+                name = "name $sample",
+                yearString = "yearString $sample",
+                address = "address $sample",
+                type = "distance $sample",
+                mass = "mass",
+                reclat = 0.0,
+                reclong = 0.0
+            )
         )
-    )
+    }
 }
-
-
-@Preview("MeteoriteDetail no Address Light")
-@Composable
-fun MeteoriteDetailNoAddressLight() {
-    val sample = "test"
-    MeteoriteDetail(
-        meteoriteView = MeteoriteView(
-            id = sample,
-            name = "name $sample",
-            yearString = "yearString $sample",
-            address = "address $sample",
-            type = "distance $sample",
-            mass = "mass",
-            reclat = 0.0,
-            reclong = 0.0
-        )
-    )
-}
-
